fe4f578495ec29e7d40ac142068d129b345558fd
hiraoka120 <hiraok120@gmail.com>
2021-11-07 17:28:00 +0900

    set focus

--- src/components/AnswerText/index.tsx
+++ src/components/AnswerText/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useRef, useEffect } from 'react';
 import { Style } from './styles.css'
 
 export type Props = {
@@ -10,11 +10,21 @@ export const AnswerText = ( {
   answer,
   handleChange
 }: Props ): JSX.Element => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const node = inputRef.current;
+    if (node) {
+      node.focus();
+    }
+  }, [answer]);
+
   return (
     <input
       className={Style}
       onChange={handleChange}
       value={answer}
+      ref={inputRef}
       type="text"
     />
   );
